3f809e14a7d5bc481a28efea029dde4e0480ccf8
tulio.fergulha <tfergulha@parceirosbrk.com.br>
2022-07-19 22:30:57 -0300

    Add client gRPC

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/tuliofergulha/fullcycle-grpc/pb"
 	"github.com/tuliofergulha/fullcycle-grpc/services"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -18,6 +19,7 @@ func main() {
 
 	grpcServer := grpc.NewServer()
 	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
+	reflection.Register(grpcServer)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Could not serve: %v", err)
--- go.mod
+++ go.mod
@@ -6,3 +6,12 @@ require (
 	google.golang.org/grpc v1.48.0
 	google.golang.org/protobuf v1.28.0
 )
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	golang.org/x/net v0.0.0-20220412020605-290c469a71a5 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220407144326-9054f6ed7bac // indirect
+)
