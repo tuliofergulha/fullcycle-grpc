b2a3fd3a5623b8d95249f2e5638924d61f5c9a03
tulio.fergulha <tfergulha@parceirosbrk.com.br>
2022-07-19 21:13:43 -0300

    First grpc server

---
+++ cmd/server/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/tuliofergulha/fullcycle-grpc/pb"
+	"github.com/tuliofergulha/fullcycle-grpc/services"
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Could not serve: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tuliofergulha/fullcycle-grpc
 
 go 1.18
 
-require google.golang.org/protobuf v1.28.0 // indirect
+require (
+	google.golang.org/grpc v1.48.0
+	google.golang.org/protobuf v1.28.0
+)
