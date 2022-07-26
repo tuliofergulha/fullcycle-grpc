464a7e389f85cee83fab1568f0e5663284936eb8
tulio.fergulha <tfergulha@parceirosbrk.com.br>
2022-07-25 22:12:04 -0300

    Implement client and server streaming at the same time

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -21,7 +21,8 @@ func main() {
 	client := pb.NewUserServiceClient(connection)
 	// AddUser(client)
 	// AddUserVerbose(client)
-	AddUsers(client)
+	// AddUsers(client)
+	AddUserStreamBoth(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -109,4 +110,70 @@ func AddUsers(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddUserStreamBoth(client pb.UserServiceClient) {
+
+	stream, err := client.AddUserStreamBoth(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "t1",
+			Name:  "Túlio Fergulha",
+			Email: "test@test.com.br",
+		},
+		&pb.User{
+			Id:    "t2",
+			Name:  "Túlio Fergulha 2",
+			Email: "test2@test.com.br",
+		},
+		&pb.User{
+			Id:    "t3",
+			Name:  "Túlio Fergulha 3",
+			Email: "test3@test.com.br",
+		},
+		&pb.User{
+			Id:    "t4",
+			Name:  "Túlio Fergulha 4",
+			Email: "test4@test.com.br",
+		},
+		&pb.User{
+			Id:    "t5",
+			Name:  "Túlio Fergulha 5",
+			Email: "test5@test.com.br",
+		},
+	}
+
+	wait := make(chan int)
+
+	go func() {
+		for _, req := range reqs {
+			fmt.Println("Sending user: ", req.Name)
+			stream.Send(req)
+			time.Sleep(time.Second * 2)
+		}
+		stream.CloseSend()
+	}()
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+
+			if err != nil {
+				log.Fatalf("Error receiving data: %v", err)
+				break
+			}
+
+			fmt.Printf("Recebendo user %v com status %v \n", res.GetUser().GetName(), res.GetStatus())
+		}
+		close(wait)
+	}()
+
+	<-wait
 }
--- services/user.go
+++ services/user.go
@@ -97,7 +97,30 @@ func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
 			Name:  req.GetName(),
 			Email: req.GetEmail(),
 		})
-		fmt.Println("Adding", req.GetName())
 
+		fmt.Println("Adding ", req.GetName())
+	}
+}
+
+func (*UserService) AddUserStreamBoth(stream pb.UserService_AddUserStreamBothServer) error {
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			log.Fatalf("Error receiving stream from the client: %v", err)
+		}
+
+		err = stream.Send(&pb.UserResultStream{
+			Status: "Added",
+			User:   req,
+		})
+
+		if err != nil {
+			log.Fatalf("Error sending stream to the client: %v", err)
+		}
 	}
 }
